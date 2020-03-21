b648a66948008ef2241d31eac571a4715ea59277
Hideto Obara <hidetobara@gmail.com>
2020-03-21 23:38:42 +0900

    dht12が動くようになった！

--- obniz/batch.py
+++ obniz/batch.py
@@ -3,49 +3,89 @@ import asyncio
 from obniz import Obniz
 
 
-class Manager:
-    def __init__(self):
-        self.obniz = Obniz('0167-3051')
+class DeviceWithObniz:
+    """
+    Obnizを使ったデバイス制御
+    """
 
-    def set_lux(self):
-        async def on_lux(obniz):
-            obniz.io0.output(False)
-            obniz.ad1.start()
-            obniz.io2.output(True)
+    def __init__(self, obid='0167-3051'):
+        self.obniz = Obniz(obid)
+        self.data = {}
+
+    def store(self, name, value):
+        if name not in self.data:
+            self.data[name] = []
+        self.data[name].append(value)
+
+    def calculate_median(self, name):
+        if name not in self.data:
+            return None
+        array = sorted(self.data[name])
+        return array[ int(len(array)/2) ]
+
+    def get_humidity(self):
+        return self.calculate_median("humidity")
+    def get_temperature(self):
+        return self.calculate_median("temperature")
+    def get_lux(self):
+        return self.calculate_median("lux")
+
+    def run_power(self, obniz, gnd, vdd):
+        if vdd is not None:
+            #obniz.__dict__['io{}'.format(vdd)].drive("5v") # obniz bug ?
+            obniz.__dict__['io{}'.format(vdd)].output(True)
+        if gnd is not None:
+            obniz.__dict__['io{}'.format(gnd)].output(False)
+
+    def set_tept4400(self, ad=1, gnd=0, vdd=2):
+        def calculate_lux(vol):
+            if vol < 0.0: return 0
+            lux = 7232.4 * vol + 2.5
+            return math.floor(lux / 100) * 100
+
+        async def on_tept4400(obniz):
+            self.run_power(gnd, vdd)
+            land = obniz.__dict__['ad{}'.format(ad)]
+            land.start()
+            drain = obniz.__dict__['ad{}'.format(vdd)]
+            drain.start()
+            await obniz.wait(3000)
             for _ in range(0, 10):
+                print("tept4400", land.value, drain.value)
+                self.store("lux", calculate_lux(drain.value - land.value))
                 await obniz.wait(1000)
-                print("voltage=", obniz.ad1.value)
-            obniz.io2.end()
             obniz.close()
             asyncio.get_event_loop().stop()
-        self.obniz.onconnect = on_lux
+        self.obniz.onconnect = on_tept4400
         return self
 
-    def set_dht12(self):
+    def set_dht12(self, sda=7, scl=9, gnd=11, vdd=5):
         # do not run
         async def on_dht12(obniz):
-            obniz.io5.drive("5v")
-            obniz.io5.output(True)
-            obniz.io11.output(False)
+            self.run_power(obniz, gnd, vdd)
             await obniz.wait(3000)
             i2c = obniz.i2c0
-            i2c.start( {"mode":"master", "sda":7, "scl":9, "clock":100000, "pull":"5v"} )
+            i2c.start( {"mode":"master", "sda":sda, "scl":scl, "clock":100000, "pull":"5v"} )
             i2c.onerror = lambda e: print("dht12", e)
             for _ in range(0, 10):
+                i2c.write(0x5c, [0x00])
                 r = await i2c.read_wait(0x5c, 5)
                 chk = (r[0] + r[1] + r[2] + r[3]) & 0xff
                 print("dht=", r, "chk=", chk)
+                if chk != r[4]: continue
+                self.store("humidity", r[0] + r[1]*0.1)
+                self.store("temperature", r[2] + r[3]*0.1)
                 await obniz.wait(1000)
             asyncio.get_event_loop().stop()
         self.obniz.onconnect = on_dht12
         return self
 
-    def set_tsl2561(self):
+    def set_tsl2561(self, sda=1, scl=0, gnd=2, vdd=3):
         GAINS = {0x00: 16.0*322.0/11.0, 0x01: 16.0*322.0/81.0, 0x02: 16.0*1.0, 0x12: 1.0*1.0}
-        gain = 0x00 # Need modify gain.
+        GAIN = 0x00 # Need modify gain.
         def getScale():
-            return GAINS[gain]
-        def compute_lux(int0, int1):
+            return GAINS[GAIN]
+        def calculate_lux(int0, int1):
             ch0 = int0 * getScale()
             ch1 = int1 * getScale()
 
@@ -63,15 +103,13 @@ class Manager:
             return lux
 
         async def on_tsl2561(obniz):
-            obniz.io3.drive("5v") # 3v is better, but do not run.
-            obniz.io3.output(True)
-            obniz.io2.output(False)
+            self.run_power(obniz, gnd, vdd)
             await obniz.wait(3000)
             i2c = obniz.i2c0
-            i2c.start( {"mode":"master", "sda":1, "scl":0, "clock":100000, "pull":"5v"} )
+            i2c.start( {"mode":"master", "sda":sda, "scl":scl, "clock":100000, "pull":"5v"} )
             i2c.onerror = lambda e: print("tsl2561", e)
             i2c.write(0x39, [0x80, 0x03])
-            i2c.write(0x39, [0x81, 0x00])
+            i2c.write(0x39, [0x81, GAIN])
             for _ in range(0, 10):
                 i2c.write(0x39, [0xAC])
                 c0 = await i2c.read_wait(0x39, 2)
@@ -79,7 +117,9 @@ class Manager:
                 i2c.write(0x39, [0xAE])
                 c1 = await i2c.read_wait(0x39, 2)
                 ch1 = c1[1] * 256 + c1[0]
-                print("tsl2561=", ch0, ch1, compute_lux(ch0, ch1))
+                print("tsl2561=", ch0, ch1)
+                self.store("lux", calculate_lux(ch0, ch1))
+                await obniz.wait(1000)
             asyncio.get_event_loop().stop()
         self.obniz.onconnect = on_tsl2561
         return self
@@ -97,6 +137,9 @@ if __name__ == "__main__":
     parser.add_argument('--on', action='store_true')
     args = parser.parse_args()
 
-    m = Manager()
+    d = DeviceWithObniz()
     if args.on:
-        m.set_tsl2561().run()
+        d.set_dht12().run()
+        print("temperature=", d.get_temperature())
+        print("humidity=", d.get_humidity())
+        print("lux=", d.get_lux())
